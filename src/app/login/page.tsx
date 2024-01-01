ff46f175b615b13200238807d77744686cf9566b
codephenix4 <yahia19raouf@gmail.com>
2024-01-01 17:51:57 +0100

    fix Images

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -27,11 +27,13 @@ export function ConnexionPage() {
   return (
     <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-        <Image
+        {/* <Image
           className="mx-auto h-10 w-auto"
           src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
           alt="Your Company"
-        />
+          width={5}
+          height={5}
+        /> */}
         <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
           LogIn
         </h2>
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -37,11 +37,13 @@ export function RegisterPage() {
   return (
     <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-        <Image
+        {/* <Image
           className="mx-auto h-10 w-auto"
           src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
           alt="Your Company"
-        />
+          width={5}
+          height={5}
+        /> */}
         <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
           Inscription
         </h2>
@@ -118,7 +120,7 @@ export function RegisterPage() {
               type="submit"
               className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
-              S'inscrire
+              S&quotinscrire
             </button>
           </div>
         </form>
